6f5e24a8df0ef7623d96c1e8681f7eb72fb893f1
Taylor Williams <taylor@thewilliams.net>
2019-02-03 07:53:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function calculatesFarePrice(feetTravelled){
   let fare = 0;
   switch (true) {
     case feetTravelled > 2500:
-      fare = 'cannot travel that far'
+      fare = 'cannot travel that far';
       break;
     case feetTravelled > 2000:
-      fare += 25
+      fare += 25;
     case feetTravelled > 400:
-      fare += Math.min(feetTravelled - 400, 1600) * 0.02
+      fare += Math.min(feetTravelled - 400, 1600) * 0.02;
   }
   return fare;
 }
