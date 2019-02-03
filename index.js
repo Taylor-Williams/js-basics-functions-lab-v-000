fb16e47127446c67c8585930bebd4f5905b48f70
Taylor Williams <taylor@thewilliams.net>
2019-02-03 07:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  return math.abs((42 - blockNumber));
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  return distanceFromHqInBlocks(blockNumber) * 264;
+}
