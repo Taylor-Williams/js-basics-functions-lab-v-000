dfbb851156715ccccbfbdd63a59dc2ec52aa0cdb
Taylor Williams <taylor@thewilliams.net>
2019-02-03 07:47:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function distanceFromHqInFeet(blockNumber) {
 function distanceTravelledInFeet(startBlock, endBlock){
   return math.abs((startBlock - endBlock)) * 264;
 }
+
+function calculatesFarePrice(feetTravelled){
+  let fare;
+  switch (true) {
+    case feetTravelled > 400:
+    
+    default:
+      
+  }
+}
